e0f2375cebd32a3d7cdeb8435bd2657401a88d06
Matthew Sim <msim@berkeley.edu>
2022-10-01 08:16:52 +0000

    finishing part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,12 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.all
+      @all_ratings = Movie.all_ratings
+      @ratings_to_show = []
+      if params[:ratings].nil? == false
+        @ratings_to_show = params[:ratings].keys
+      end
+      @movies = Movie.with_ratings(@ratings_to_show)
     end
   
     def new
@@ -44,4 +49,13 @@ class MoviesController < ApplicationController
     def movie_params
       params.require(:movie).permit(:title, :rating, :description, :release_date)
     end
+
+   
+
+
+
+
+      
+
+
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.with_ratings(ratings_list)
+    
+    if(ratings_list.length == 0)
+      return Movie.all
+    end
+    return Movie.where(rating: ratings_list)
+  end
+
+
+  def self.all_ratings ; ['G','PG','PG-13','R']  ; end
+  
 end
